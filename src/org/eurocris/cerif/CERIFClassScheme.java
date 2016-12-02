e549b568c29eaeef93de0d2b612b32c97a40e209
jdvorak001 <jan.dvorak@ff.cuni.cz>
2016-12-02 19:28:28 +0100

    @Retention RUNTIME

--- src/org/eurocris/cerif/CERIFClassScheme.java
+++ src/org/eurocris/cerif/CERIFClassScheme.java
@@ -6,10 +6,13 @@ package org.eurocris.cerif;
 import static java.lang.annotation.ElementType.TYPE;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Documented
 @Target( TYPE )
+@Retention( RetentionPolicy.RUNTIME )
 /**
  * A CERIF Classification Scheme.
  * @author jdvorak
