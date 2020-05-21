fa9da779effa2094cfd3f068ea9b7b8cc379a46e
Jan Dvorak <jdvorak001@users.noreply.github.com>
2020-05-22 00:55:46 +0200

    take the encryption variable base name from $1

--- CERIF-Publishing/travis-publish.sh
+++ CERIF-Publishing/travis-publish.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # Publish contents under cerif.eurocris.org/
-# Usage: ./travis-publish.sh [file|directory ...]
+# Usage: ./travis-publish.sh varbasename [file|directory ...]
 
-openssl aes-256-cbc -K $encrypted_9f5c09747267_key -iv $encrypted_9f5c09747267_iv -in .ssh/cerif.eurocris.org.key.enc -out .ssh/cerif.eurocris.org.key -d && \
+enc_varname=encrypted_"$1"_key
+iv_varname=encrypted_"$1"_iv
+key_filename=.ssh/cerif.eurocris.org.key-"$1".enc
+shift
+openssl aes-256-cbc -K "${!enc_varname}" -iv "${!iv_varname}" -in "$key_filename" -out .ssh/cerif.eurocris.org.key -d && \
 rsync -e 'ssh -i .ssh/cerif.eurocris.org-2.key -l jdvorak' -crz --delay-updates --delete-after --dry-run --itemize-changes "$@" www.eurocris.org:/data-eurocris/documentRoot/wwwcerif/
